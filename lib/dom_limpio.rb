0ea909820a6cc86f2684308e7862404466759101
Victor Ortiz <kespers@gmail.com>
2016-08-06 14:36:38 +0200

    Fix sprockets-gem

--- lib/dom_limpio.rb
+++ lib/dom_limpio.rb
@@ -1,6 +1,37 @@
-require "dom_limpio/version"
-require "dom_limpio/engine"
-
 module DomLimpio
-  # Your code goes here...
+  class << self
+    def load!
+      if defined?(::Rails)
+        register_rails_engine
+      elsif defined?(::Sprockets)
+        register_sprockets
+      end
+      configure_sass
+    end
+
+    def assets_path
+      @gem_path ||= File.expand_path "../assets", File.dirname(__FILE__)
+    end
+
+    def stylesheets_path
+      File.join assets_path, 'stylesheets'
+    end
+
+    private
+
+    def configure_sass
+      require 'sass'
+      ::Sass.load_paths << stylesheets_path
+    end
+
+    def register_rails_engine
+      require "dom_limpio/engine"
+    end
+
+    def register_sprockets
+      Sprockets.append_path(stylesheets_path)
+    end
+  end
 end
+
+DomLimpio.load!
--- lib/dom_limpio/engine.rb
+++ lib/dom_limpio/engine.rb
@@ -1,10 +1,9 @@
 module DomLimpio
   module Rails
     class Engine < ::Rails::Engine
-      initializer 'dom_limpio' do | app |
-        dl_path = File.expand_path("../../../src/css", __FILE__)
-        app.config.assets.paths << dl_path
-        app.middleware.use ::ActionDispatch::Static, "#{root}/src/css"
+      initializer 'dom-limpio' do |app|
+        app.config.assets.paths << DomLimpio.stylesheets_path
+        app.middleware.use ::ActionDispatch::Static, DomLimpio.assets_path
       end
     end
   end
